d7d68a139295912e2992e20c6f4d46c45048e947
Chinthaka@21022 <chinthakal@aplicy.com>
2025-04-09 17:05:24 +0530

    change endpoint file route

--- src/api/axios/endpoint.ts
+++ src/api/axios/endpoint.ts
@@ -1,6 +1,6 @@
 export const Path = {
-  service: "service",
-  staff: "staff",
-  category: "category",
-  subcategory: "subcategory",
+  service: "service/api/v1/service",
+  staff: "service/api/v1/staff",
+  category: "service/api/v1/category",
+  subcategory: "service/api/v1/subcategory",
 };
